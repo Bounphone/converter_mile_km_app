4bef9d4201889c3ba4315bbd8b5eccef2220a7ec
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2023-01-09 14:52:31 +0700

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -23,75 +23,77 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 15,
-            ),
-            TextField(
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ], // Only numbers can be entered
-              keyboardType: TextInputType.number,
-              onChanged: (val) {
-                /// if there are input
-                if (input.text.isNotEmpty) {
-                  setState(() {
-                    if (groupVal == DataType.KM) {
-                      result = double.parse(input.text) * kmToMile;
-                    } else {
-                      result = double.parse(input.text) * mileToKM;
-                    }
-                  });
-                } else {
-                  setState(() {
-                    input.clear();
-                    result = 0;
-                  });
-                }
-              },
-              controller: input,
-              decoration: InputDecoration(
-                  hintText: groupVal == DataType.KM
-                      ? 'Please enter KM'
-                      : 'Please enter Mile'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            RadioListTile(
-                title: Text('KM'),
-                value: DataType.KM,
-                groupValue: groupVal,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ], // Only numbers can be entered
+                keyboardType: TextInputType.number,
                 onChanged: (val) {
-                  setState(() {
-                    groupVal = DataType.KM;
-                    input.clear();
-                    result = 0;
-                  });
-                }),
-            RadioListTile(
-                title: Text('Mile'),
-                value: DataType.Mile,
-                groupValue: groupVal,
-                onChanged: (val) {
-                  setState(() {
-                    groupVal = DataType.Mile;
-                    input.clear();
-                    result = 0;
-                  });
-                }),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              '${result == 0.0 ? '' : '${input.text} ${groupVal == DataType.KM ? 'KM' : 'Miles'} is ${result.toStringAsFixed(2)} ${groupVal == DataType.KM ? 'Miles' : 'KM'}'}',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-            )
-          ],
+                  /// if there are input
+                  if (input.text.isNotEmpty) {
+                    setState(() {
+                      if (groupVal == DataType.KM) {
+                        result = double.parse(input.text) * kmToMile;
+                      } else {
+                        result = double.parse(input.text) * mileToKM;
+                      }
+                    });
+                  } else {
+                    setState(() {
+                      input.clear();
+                      result = 0;
+                    });
+                  }
+                },
+                controller: input,
+                decoration: InputDecoration(
+                    hintText: groupVal == DataType.KM
+                        ? 'Please enter KM'
+                        : 'Please enter Mile'),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              RadioListTile(
+                  title: Text('KM'),
+                  value: DataType.KM,
+                  groupValue: groupVal,
+                  onChanged: (val) {
+                    setState(() {
+                      groupVal = DataType.KM;
+                      input.clear();
+                      result = 0;
+                    });
+                  }),
+              RadioListTile(
+                  title: Text('Mile'),
+                  value: DataType.Mile,
+                  groupValue: groupVal,
+                  onChanged: (val) {
+                    setState(() {
+                      groupVal = DataType.Mile;
+                      input.clear();
+                      result = 0;
+                    });
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                '${result == 0.0 ? '' : '${input.text} ${groupVal == DataType.KM ? 'KM' : 'Miles'} is ${result.toStringAsFixed(2)} ${groupVal == DataType.KM ? 'Miles' : 'KM'}'}',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+              )
+            ],
+          ),
         ),
       ),
     );
